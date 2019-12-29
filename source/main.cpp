9f81b068f298d633cfc2839a080b9ffa4f615151
Daniel Duller <46626392+dadul96@users.noreply.github.com>
2019-12-29 21:59:06 +0100

    Add project files.

---
+++ source/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char* strA = {"#include <stdio.h>\n#include <string.h>\n\nint main(void) {\n    char* strA = {\"\"};\n    char* strB = malloc(2000);\n    char strC[4000];\n    int x = 76;\n    strncpy(strC, strA, x);\n    strC[x] = 0;\n    int counterA = 0;\n    int counterB = 0;\n    while (strA[counterA] != 0)\n    {\n        if (strA[counterA] == 10)\n        {\n            strB[counterB] = 92;\n            counterB = counterB + 1;\n            strB[counterB] = 110;\n        }\n        else if (strA[counterA] == 34)\n        {\n            strB[counterB] = 92;\n            counterB = counterB + 1;\n            strB[counterB] = 34;\n        }\n        else\n        {\n            strB[counterB] = strA[counterA];\n        }\n        counterA = counterA + 1;\n        counterB = counterB + 1;\n    }\n    strB[counterB] = 0;\n    strcat(strC, strB);\n    strcat(strC, strA + x);\n    printf(strC);\n    free(strB);\n    return 0;\n}"};
+    char* strB = malloc(2000);
+    char strC[4000];
+    int x = 76;
+    strncpy(strC, strA, x);
+    strC[x] = 0;
+    int counterA = 0;
+    int counterB = 0;
+    while (strA[counterA] != 0)
+    {
+        if (strA[counterA] == 10)
+        {
+            strB[counterB] = 92;
+            counterB = counterB + 1;
+            strB[counterB] = 110;
+        }
+        else if (strA[counterA] == 34)
+        {
+            strB[counterB] = 92;
+            counterB = counterB + 1;
+            strB[counterB] = 34;
+        }
+        else
+        {
+            strB[counterB] = strA[counterA];
+        }
+        counterA = counterA + 1;
+        counterB = counterB + 1;
+    }
+    strB[counterB] = 0;
+    strcat(strC, strB);
+    strcat(strC, strA + x);
+    printf(strC);
+    free(strB);
+    return 0;
+}
