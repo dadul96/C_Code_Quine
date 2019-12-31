7392f60e9d079b2947fcd6e34f244a284a7e9450
Daniel Duller <46626392+dadul96@users.noreply.github.com>
2019-12-31 11:23:23 +0100

    Add stdlib.h

--- source/main.c
+++ source/main.c
@@ -1,11 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
-    char* strA = {"#include <stdio.h>\n#include <string.h>\n\nint main(void) {\n    char* strA = {\"\"};\n    char* strB = malloc(2000);\n    char strC[4000];\n    int x = 76;\n    strncpy(strC, strA, x);\n    strC[x] = 0;\n    int counterA = 0;\n    int counterB = 0;\n    while (strA[counterA] != 0)\n    {\n        if (strA[counterA] == 10)\n        {\n            strB[counterB] = 92;\n            counterB = counterB + 1;\n            strB[counterB] = 110;\n        }\n        else if (strA[counterA] == 34)\n        {\n            strB[counterB] = 92;\n            counterB = counterB + 1;\n            strB[counterB] = 34;\n        }\n        else\n        {\n            strB[counterB] = strA[counterA];\n        }\n        counterA = counterA + 1;\n        counterB = counterB + 1;\n    }\n    strB[counterB] = 0;\n    strcat(strC, strB);\n    strcat(strC, strA + x);\n    printf(strC);\n    free(strB);\n    return 0;\n}"};
+    char* strA = {"#include <stdlib.h>\n#include <stdio.h>\n#include <string.h>\n\nint main(void) {\n    char* strA = {\"\"};\n    char* strB = malloc(2000);\n    char strC[4000];\n    int x = 96;\n    strncpy(strC, strA, x);\n    strC[x] = 0;\n    int counterA = 0;\n    int counterB = 0;\n    while (strA[counterA] != 0)\n    {\n        if (strA[counterA] == 10)\n        {\n            strB[counterB] = 92;\n            counterB = counterB + 1;\n            strB[counterB] = 110;\n        }\n        else if (strA[counterA] == 34)\n        {\n            strB[counterB] = 92;\n            counterB = counterB + 1;\n            strB[counterB] = 34;\n        }\n        else\n        {\n            strB[counterB] = strA[counterA];\n        }\n        counterA = counterA + 1;\n        counterB = counterB + 1;\n    }\n    strB[counterB] = 0;\n    strcat(strC, strB);\n    strcat(strC, strA + x);\n    printf(strC);\n    free(strB);\n    return 0;\n}"};
     char* strB = malloc(2000);
     char strC[4000];
-    int x = 76;
+    int x = 96;
     strncpy(strC, strA, x);
     strC[x] = 0;
     int counterA = 0;
